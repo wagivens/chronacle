4bf2ee7f14652574ab68068fe016c35d7a5a49cd
Willard Givens IV <willard.givensiv@gmail.com>
2021-11-13 16:38:05 -0800

    Chore: configure webpack entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-module.exports = {
-    mode: "production",
-    entry: {
+const path = require('path');
 
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: './src/index.js',
     }, 
     output: {
-        
+		filename: '[name].[contentHash].js',
+		path: path.resolve(__dirname, 'dist')
     }
 }
