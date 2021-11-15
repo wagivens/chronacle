cf0a866d46759910c7fcdadc920e6b05045ae5b2
Willard Givens IV <willard.givensiv@gmail.com>
2021-11-14 21:53:48 -0800

    Chore: install postcss

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
+    
     entry: {
         main: './src/index.ts',
     }, 
+    
     output: {
-		filename: '[name].[contentHash].js',
-		path: path.resolve(__dirname, 'dist')
+		  filename: '[name].[contentHash].js',
+		  path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['postcss-loader']
+        }
+      ]
     }
 }
