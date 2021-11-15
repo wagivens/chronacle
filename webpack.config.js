fd333cb9429eaf6039cdcbca4abd83f809b88b35
Willard Givens IV <willard.givensiv@gmail.com>
2021-11-14 22:32:03 -0800

    Chore: install mini-css-extract-plugin for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -12,12 +13,16 @@ module.exports = {
 		  path: path.resolve(__dirname, 'dist')
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     module: {
       rules: [
         {
           test: /\.scss$/,
           exclude: /node_modules/,
-          use: ['postcss-loader', 'css-loader']
+          use: [MiniCssExtractPlugin.loader({
+            filename: 'main_styles.css'
+          }), 'postcss-loader', 'css-loader'],
         }
       ]
     }
