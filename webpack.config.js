5f952d77a179b4476fb6e286d315e39671d0853e
Willard Givens IV <willard.givensiv@gmail.com>
2021-11-16 14:35:18 -0800

    Chore: install postcss and postcss-cli

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,34 @@ module.exports = {
     mode: 'production',
     
     entry: {
-        main: './src/index.ts',
+        main: './src/index.js',
     }, 
     
     output: {
 		  filename: '[name].js',
-		  path: path.resolve(__dirname, 'dist')
+		  path: path.resolve(__dirname, 'dist/')
     },
 
-    plugins: [new MiniCssExtractPlugin()],
+    // 1. Use Sass command to output scss to the dist folder as regular css
+      // OR
+    // 2. process scss with postcss, use the css/style loaders, use miniCSSExtractPlugin
+      // to take the source css and create a file in the dist folder with the final css.
+      // OR
+    // 3. Use the postcss-cli on the files in src/scss, then use css-loader,
+      // style-loader and MiniCssExtractPlugin to give us a compiled CSS 
+      // file in the dist folder
 
     module: {
       rules: [
         {
           test: /\.scss$/,
           exclude: /node_modules/,
-          use: [MiniCssExtractPlugin.loader({
-            filename: 'main_styles.css'
-          }), 'postcss-loader', 'css-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
         }
       ]
-    }
+    },
+
+    plugins: [new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    })],
 }
