55d43dcff6cc3f86c5ccbb8b9f12f3662248d393
Willard Givens IV <willard.givensiv@gmail.com>
2021-11-16 16:31:55 -0800

    Chore: install postcss-preset-env and update npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -13,19 +13,10 @@ module.exports = {
 		  path: path.resolve(__dirname, 'dist/')
     },
 
-    // 1. Use Sass command to output scss to the dist folder as regular css
-      // OR
-    // 2. process scss with postcss, use the css/style loaders, use miniCSSExtractPlugin
-      // to take the source css and create a file in the dist folder with the final css.
-      // OR
-    // 3. Use the postcss-cli on the files in src/scss, then use css-loader,
-      // style-loader and MiniCssExtractPlugin to give us a compiled CSS 
-      // file in the dist folder
-
     module: {
       rules: [
         {
-          test: /\.scss$/,
+          test: /\.css$/,
           exclude: /node_modules/,
           use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
         }
@@ -33,6 +24,6 @@ module.exports = {
     },
 
     plugins: [new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: '[name].css',
     })],
 }
