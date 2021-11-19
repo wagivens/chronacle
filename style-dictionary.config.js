1687bb1efbae3f8b80e1f37a669e6c6c3e4d0620
Willard Givens IV <willard.givensiv@gmail.com>
2021-11-18 16:41:01 -0800

    Style: add starter design tokens for color

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,58 +1,19 @@
 const StyleDictionary = require('style-dictionary');
-const transformer = StyleDictionary.transform['attribute/cti'].transformer;
-
-const propertiesToCTI = {
-  'width': {category: 'size', type: 'dimension'},
-  'min-width': {category: 'size', type: 'dimension'},
-  'max-width': {category: 'size', type: 'dimension'},
-  'height': {category: 'size', type: 'dimension'},
-  'min-height': {category: 'size', type: 'dimension'},
-  'max-height': {category: 'size', type: 'dimension'},
-  'border-width': {category: 'size', type: 'border', item: 'width'},
-  'border-radius': { category: 'size', type: 'border', item: 'width' },
-  'border-color': {category: 'color', type: 'border'},
-  'background-color': {category: 'color', type: 'background'},
-  'color': {category: 'color', type: 'font'},
-  'text-color': { category: 'color', type: 'font' },
-  'padding': {category: 'size', type: 'padding'},
-  'padding-vertical': {category: 'size', type: 'padding'},
-  'padding-horizontal': {category: 'size', type: 'padding'},
-  'icon': {category: 'content', type: 'icon'},
-  'font-size': {category: 'size', type: 'font'},
-  'line-height': { category: 'size', type: 'line-height' },
-  'size': {category: 'size', type: 'icon'}
-}
-
-const CTITransform = {
-  type: `attribute`,
-  transformer: (prop) => {
-    // Only do this custom functionality in the 'component' top-level namespace.
-    if (prop.path[0] === 'component') {
-      // When defining component tokens, the key of the token is the relevant CSS property
-      // The key of the token is the last element in the path array
-      return propertiesToCTI[prop.path[prop.path.length - 1]];
-    } else {
-      // Fallback to the original 'attribute/cti' transformer
-      return transformer(prop);
-    }
-  }
-}
 
 module.exports = {
-    transform: {
-        // Override the attribute/cti transform
-        'attribute/cti': CTITransform
-    },
-    source: ['tokens/**/*.json'],
+    source: ['src/tokens/**/*.json'],
     platforms: {
-      scss: {
-        transformGroup: 'scss',
-        buildPath: 'src/scss/',
-        files: [{
-          destination: '_variables.scss',
-          format: 'scss/variables'
-        }],
-        actions: ['copy_assets']
-      }
+        scss: {
+            transforms: ["attribute/cti", "name/cti/camel", "color/hsl", "size/rem"],
+            transformGroup: 'scss',
+            buildPath: 'src/scss/',
+            files: [{
+                destination: '_variables.scss',
+                format: 'scss/variables',
+                options: {
+                    outputReferences: true,
+                }
+            }],
+        }
     }
 }
