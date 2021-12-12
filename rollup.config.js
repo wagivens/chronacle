027f90b3dad359dfda500c85bc7e8e0be09740ce
Willard Givens IV <willard.givensiv@gmail.com>
2021-12-11 17:23:28 -0800

    Chore: remove packages list from rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,5 @@
 const path = require('path');
 
-/* 
-Packages to add:
-    1. storybook
-    2. rollup-plugin-minify-html-literals
-    3. rollup-plugin-terser
-    4. postcss-bem-linter (for linting the SUIT css naming convention)
-*/
-
 export default {
     input: 'src/index.js',
     output: {
