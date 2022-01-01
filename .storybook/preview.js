8c171a0212856cc0a6e7e5de713eb332b31822aa
Willard Givens IV <willard.givensiv@gmail.com>
2021-12-31 23:41:42 -0800

    Style: configure UI theme for storybook docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import chronacle_theme from './chronacle_theme';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +7,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  docs: {
+    theme: chronacle_theme,
   },
 }
