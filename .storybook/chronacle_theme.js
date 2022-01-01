2a425b0d343c010d9c2fb1b9b2b306ca9f536ecd
Willard Givens IV <willard.givensiv@gmail.com>
2021-12-31 23:37:07 -0800

    Style: configure UI settings for storybook

--- .storybook/chronacle_theme.js
+++ .storybook/chronacle_theme.js
@@ -6,12 +6,15 @@ export default create({
     brandTitle: 'Chronacle',
     brandUrl: 'https://www.chronacle.app',
     brandImage: 'https://github.com/wagivens/chronacle/blob/main/assets/images/chronacle_logo.png?raw=true',
+    colorPrimary: '#409D44',
+    colorSecondary: '#0074DF',
     
     // Content Styling
     appBg: '#F2F2F2',
+    appContentBg: '#FFFFFF',
     
     // Toolbar Styling
-    barBg: '#0074DF',
+    barBg: '#004686',
     barSelectedColor: '#F2F2F2',
-    barTextColor: '#F2F2F2'
+    barTextColor: '#D5D5D5',
 });
